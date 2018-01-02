0008967965a11e062fdda677f13987496de45200
Ramesh Raju Alluri <dev.addons@gmail.com>
2018-01-02 14:52:39 +0530

    first commit

---
+++ node/express/index.js
@@ -0,0 +1,22 @@
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 80;        // set our port
+
+var router = express.Router();              // get an instance of the express Router
+router.get('/', function(req, res) {
+        console.log('Magic happens on router.get(/');
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+router.get('/ramesh', function(req, res) {
+ console.log('Magic happens on router.get(/ramesh');
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+
+app.use('/api', router);
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
